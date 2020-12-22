3ab5b672576636ef1d4225fcbb662bd9ee841306
Gyeongyeon Jeon <gjeon@c1r4s7.42seoul.kr>
2020-12-22 22:20:43 +0900

    libft_strncmp

--- libft/test/test.c
+++ libft/test/test.c
@@ -2,20 +2,24 @@
 #include <string.h>
 #include <stdlib.h>
 
-size_t ft_strlen(const char *str);
+int ft_strncmp(const char *s1, const char *s2, size_t n);
 
 int main(void)
 {
-	char *str1[30];
+	char str1[30] = "abcdefg";
+	char str2[30] = "abc0123456";
 
-	printf("strlen : %lu\n", strlen("-2147483648"));
-	printf("ft_strlen : %lu\n", ft_strlen("-2147483648"));
+	printf("strncmp : %d\n", strncmp(str1, str2, -1));
+	printf("ft_strncmp : %d\n", ft_strncmp(str1, str2, -1));
 
-	printf("strlen : %lu\n", strlen(""));
-	printf("ft_strlen : %lu\n", ft_strlen(""));
+	printf("strncmp : %d\n", strncmp(str1, str2, 0));
+	printf("ft_strncmp : %d\n", ft_strncmp(str1, str2, 0));
+	
+	printf("strncmp : %d\n", strncmp(str1, str2, 4));
+	printf("ft_strncmp : %d\n", ft_strncmp(str1, str2, 4));
 
-	printf("strlen : %lu\n", strlen("jeon\n\t"));
-	printf("ft_strlen : %lu\n", ft_strlen("jeon\n\t"));
+	printf("strncmp : %d\n", strncmp(str1, str2, 6));
+	printf("ft_strncmp : %d\n", ft_strncmp(str1, str2, 6));
 
 	return (0);
 }
