adfc9ac878784a9c610d0a080af8209272a6a3fa
Gyeongyeon Jeon <gjeon@c1r4s7.42seoul.kr>
2020-12-22 19:16:55 +0900

    libft_strlcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: gjeon <gjeon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 22:10:13 by gjeon             #+#    #+#             */
-/*   Updated: 2020/12/22 17:09:29 by gjeon            ###   ########.fr       */
+/*   Updated: 2020/12/22 19:04:25 by gjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,22 @@
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	unsigned int	i;
+	size_t	i;
 
 	if (dest == 0 || src == 0)
 		return (0);
 	i = 0;
-	while (src[i] && i < size - 1)
+	if (size > 0)
 	{
-		dest[i] = src[i];
-		i++;
+		while (src[i] && i < size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
 	}
-	dest[i] = '\0';
 	i = 0;
 	while (src[i])
 		i++;
-	if (size == 0)
-		return (i);
 	return (i);
 }
--- libft/test/test.c
+++ libft/test/test.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-#include <bsd/string.h>
-
-size_t ft_strlcpy(char *dest, const char *src, size_t size);
+#include <string.h>
+#include <stdlib.h>
 
 int main(void)
 {
-	char str1[30] = "jeon";
+	char *str1[30];
 
-	printf("strlcpy : %d\n", strlcpy(str1, "hello", 0));
+	printf("strlcpy : %lu\n", atoi());
+	printf("strlcpy : %s\n", str1);
 	
-	char str2[30] = "jeon";
+	char *str2[30];
 
-	printf("ft_strlcpy : %ld\n", ft_strlcpy(str2, "hello", 0));
+	printf("ft_strlcpy : %ld\n", ft_strlcpy(str2, "jeon gyeong yeon", 10));
+	printf("ft_strlcpy : %s\n", str2);
 	return (0);
 }
