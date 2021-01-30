7b1db361a9215bb1b23880d842c8e66a24b18366
JeonGyeongYeon <gjeon0321@gmail.com>
2021-01-30 19:18:32 +0900

    gnl_tester

--- get_next_line/test.c
+++ get_next_line/test.c
@@ -16,7 +16,7 @@ int	main()
 		free(line);
 	}
 	printf("%s\n", line);
-	//free(line);
-	system("leaks a.out > leaks_result; cat leaks_result | grep leaked && rm -rf leaks_result");
+	free(line);
+	//system("leaks a.out > leaks_result; cat leaks_result | grep leaked && rm -rf leaks_result");
 	return (0);
 }
