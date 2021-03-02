34ab246d07145a8f7877c0ae87645cc6c5252dd5
JeonGyeongYeon <gjeon0321@gmail.com>
2021-03-03 00:03:33 +0900

    ft_server

--- ft_server/srcs/run.sh
+++ ft_server/srcs/run.sh
@@ -6,15 +6,15 @@
 #    By: gjeon <gjeon@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/03/01 00:01:58 by gjeon             #+#    #+#              #
-#    Updated: 2021/03/01 07:43:43 by gjeon            ###   ########.fr        #
+#    Updated: 2021/03/02 23:41:35 by gjeon            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
-chmod 775 /tmp/run.sh
-chown -R www-data:www-data /var/www/
-chmod -R 755 /var/www/
+# chmod 775 /tmp/run.sh
+# chown -R www-data:www-data /var/www/
+# chmod -R 755 /var/www/
 
 # ssl
 openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Lee/CN=localhost" -keyout etc/ssl/private/private.key -out etc/ssl/certs/public.crt
@@ -33,17 +33,19 @@ cp -rp ./tmp/wp-config.php /var/www/html/wordpress
 
 # wordpress DB 테이블 생성
 service mysql start
-echo  "CREATE DATABASE wordpress;" | mysql -u root
+echo "CREATE DATABASE wordpress;" | mysql -u root
 echo "CREATE USER '42user'@'localhost';" | mysql -u root
 echo "SET password FOR '42user'@'localhost' = password('42seoul');    " | mysql -u root
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO '42user'@'localhost' IDENTIFIED BY '42seoul';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
 
 # phpMyAddmin
-wget https://files.phpmyadmin.net/phpMyAdmin/5.1.0/phpMyAdmin-5.1.0-all-languages.tar.gz
-mv phpMyAdmin-5.1.0-all-languages var/www/html/phpmyadmin
-rm phpMyAdmin-5.1.0-all-languages.tar.gz
-cp -rp /tmp/config.inc.php var/www/html/phpmyadmin/
+wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
+tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz
+mv phpMyAdmin-5.0.2-all-languages phpmyadmin
+mv phpmyadmin /var/www/html/
+rm phpMyAdmin-5.0.2-all-languages.tar.gz
+cp -rp /tmp/config.inc.php /var/www/html/phpmyadmin/
 
 service nginx start
 service php7.3-fpm start
